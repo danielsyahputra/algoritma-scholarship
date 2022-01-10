8e1e1861c8cc51aac7185db46fcd4096d083c5ae
danielsyahputra13 <dnlshp@gmail.com>
2022-01-10 21:46:35 +0700

    selesain materi day1 week5

--- week5/ngetes_R.R
+++ week5/ngetes_R.R
@@ -1,2 +1,11 @@
-hello <- "Hello"
-print(hello)
+categories <- factor(c("OfficeSupplies", 'Computers', 'Packaging', 'Machinery', 'Building'))
+categories
+
+# Ini dataFrames di R
+categories_df <- data.frame(categories=c("OfficeSupplies", 'Computers', 'Packaging', 'Machinery', 'Building'),
+                            category_id=c(111, 112, 113, 114, 115))
+categories_df
+
+categories_df$category_id + 1
+categories_df$categories
+categories_df
